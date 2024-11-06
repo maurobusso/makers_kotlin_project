d4195b04d91dfc0e6519b278ef1adc509fec2d24
kharileigh <kharileighmiller@gmail.com>
2024-11-06 15:28:00 +0000

    Word guesses

---
+++ app/src/main/java/com/example/hangman/Words.kt
@@ -0,0 +1,20 @@
+package com.example.hangman
+
+object EndangeredAnimals {
+    val ANIMALS = listOf(
+        "Amur Leopard", "Black Rhino", "Cross River Gorilla", "Hawksbill Turtle", "Javan Rhino",
+        "Malayan Tiger", "Mountain Gorilla", "Orangutan", "Saola", "South China Tiger",
+        "Sumatran Elephant", "Sumatran Orangutan", "Sumatran Rhino", "Vaquita", "Yangtze Finless Porpoise",
+        "African Forest Elephant", "Asian Elephant", "Blue Whale", "Bonobo", "Chimpanzee",
+        "Fin Whale", "Galápagos Penguin", "Ganges River Dolphin", "Giant Panda", "Green Sea Turtle",
+        "Hector's Dolphin", "Indian Elephant", "Indochinese Tiger", "Irawaddy Dolphin", "Leatherback Turtle",
+        "Pangolin", "Polar Bear", "Red Panda", "Sea Otter", "Snow Leopard",
+        "Sri Lankan Elephant", "Sunda Tiger", "Whale Shark", "Western Lowland Gorilla", "White Rhino",
+        "Yellow-eyed Penguin", "African Penguin", "Albatross", "Andean Condor", "Axolotl",
+        "Black-footed Ferret", "Bornean Orangutan", "California Condor", "Chinese Pangolin", "Dugong",
+        "Eastern Gorilla", "Ethiopian Wolf", "Hainan Gibbon", "Hirola", "Iberian Lynx",
+        "Irrawaddy Dolphin", "Kakapo", "Kemp's Ridley Sea Turtle", "Mexican Wolf", "Monarch Butterfly",
+        "North Atlantic Right Whale", "Northern Hairy-nosed Wombat", "Philippine Eagle", "Puerto Rican Parrot",
+        "Pygmy Hog", "Red Wolf", "Siamese Crocodile", "Yangtze Giant Softshell Turtle", "Yellowfin Tuna"
+    )
+}

57c1acd788cc19e2b81502c99ccd0f5990a498f1
Alexia-May <alexia.maynart@gmail.com>
2024-11-07 11:33:35 +0000

    variables and binding

--- app/src/main/java/com/example/hangman/MainActivity.kt
+++ app/src/main/java/com/example/hangman/MainActivity.kt
@@ -16,25 +16,47 @@ import androidx.core.content.res.ResourcesCompat
 import com.example.hangman.ui.theme.HangmanTheme
 
 class MainActivity : ComponentActivity() {
+    // making the app visible
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//    }
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
         setContentView(R.layout.activity_main)
     }
+
+
+    // variables
+    private var attempts = 0 // falseCount
+    private var wordFoundFlag = true // gameOverFlag did they find the word?
+    private lateinit var wordToGuess:String // word to guess
+    private lateinit var visibleWord:String // targetWord visible to the player
+    private lateinit var indexes:MutableList<Int>
+    private var randomNumber=0 // index of the randomly selected word in he words file
+
+    //function to start the game and reinitialise variables
+    private fun StartGame() {
+        attempts = 0
+        binding.flower.setImageResource(0)
+
+    }
+
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     HangmanTheme {
-        Greeting("Android")
+
     }
 }
